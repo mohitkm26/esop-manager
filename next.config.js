a4af5dce57d6ef5afe218649bb2a33e5b5823f62
mohitkm26 <mohitkm26@gmail.com>
2026-02-25 02:09:55 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ['@react-pdf/renderer'] },
-  images: { domains: ['lh3.googleusercontent.com'] }
-}
-module.exports = nextConfig
